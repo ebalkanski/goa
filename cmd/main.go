2a30754b3b4206ed71985f7269116963dd217917
Emiliyan Balkanski <emilian.balkanski@gmail.com>
2021-05-06 18:55:40 +0300

    ping mongo

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/kelseyhightower/envconfig"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"github.com/ebalkanski/goa/gen/play"
 	"github.com/ebalkanski/goa/internal/service"
@@ -28,8 +29,11 @@ func main() {
 	// Init Mongo client
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.Mongo.URI))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.Mongo.URI))
 	if err != nil {
+		log.Fatal(err)
+	}
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
 		log.Fatal(err)
 	}
 
